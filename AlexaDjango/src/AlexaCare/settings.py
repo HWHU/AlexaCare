a00584b344a85943290e2b8e8bf9d906b74df055
satchelfrench <frenchsatchel@gmail.com>
2016-10-23 20:44:14 -0400

    clean up, env file

--- AlexaDjango/src/AlexaCare/settings.py
+++ AlexaDjango/src/AlexaCare/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from decouple import config
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = '4-%q5akeihppa)&dfst&gm^+6+pkmjlv)b!z7(^#*+0p@ao@*_'
-DEBUG=True
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 
 ALLOWED_HOSTS = []
